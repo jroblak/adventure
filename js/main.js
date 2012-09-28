30141ea6ea2f61a72103a074cca4b0c1e8c2a78f
Justin Oblak <jroblak@gmail.com>
2012-09-27 20:31:38 -0400

    add some code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 // main.js
+var g_resources = [{
+		name: 'map1tiles',
+		type: 'image',
+		src: 'img/map1tiles.png'
+	}, {
+		name: 'map1',
+		type: 'tmx',
+		src: 'tiles/map1.tmx'
+}];
+
+var jsApp = {
+	
+	onload: function() {
+		if (!me.video.init('jsapp', 640, 480, false, 1.0)) {
+			console.log('browser doesn\'t support html 5 canvas');
+			return;
+		}
+		
+		me.audio.init('mp3, ogg');
+		me.loader.onload = this.loaded.bind(this);
+		me.loader.preload(g_resources);
+		me.state.change(me.state.LOADING);
+	},
+	
+	loaded: function() {
+		me.state.set(me.state.PLAY, new PlayScreen());
+		me.state.change(me.state.PLAY);
+	}
+
+};
+
+var PlayScreen = me.ScreenObject.extend({
+	
+	onResetEvent: function() {
+		me.levelDirector.loadLevel('map1');
+	},
+	
+	onDestroyEvent: function() {
+		//placeholder
+	}
+});
+
+window.onReady(function() {
+	jsApp.onload();
+});
