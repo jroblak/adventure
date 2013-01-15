a41fc92709961e6d3b57d8daf83c5083c266bc5f
Justin Oblak <jroblak@gmail.com>
2013-01-14 20:14:35 -0500

    added credits

--- js/otherents.js
+++ js/otherents.js
@@ -70,6 +70,16 @@ game.EventEntity = me.LevelEntity.extend({
 			if(this.event === 'remove') {
 				obj.equippedGear = null;
 				obj.removeCompositionItem(this.item);
+			} else if (this.event === 'theend') {
+				var credits = new game.CreditsEntity(obj.pos.x, obj.pos.y);
+				me.game.add(credits, 3);
+				me.game.sort();
+				me.input.unbindKey(me.input.KEY.LEFT, "left");
+				me.input.unbindKey(me.input.KEY.RIGHT, "right");
+				me.input.unbindKey(me.input.KEY.X, "jump", true);
+				me.input.unbindKey(me.input.KEY.Z, "attack");
+				me.input.unbindKey(me.input.KEY.C, "switch", true);
+				me.input.unbindKey(me.input.KEY.SPACE, "fly");
 			}
 			this.collidable = false;
 			me.game.remove(this);
@@ -99,4 +109,36 @@ game.HealthObject = me.HUD_Item.extend({
 			context.drawImage(this.image,this.pos.x+(i*32),this.pos.y);
 		}
 	}	
+});
+
+game.CreditsEntity = me.ObjectEntity.extend({
+	init: function(x, y) {
+		var self = this;
+		self.parent(x, y, {image: "explode", spritewidth: 1});
+
+		self.credits1 = "      The End";
+		self.credits2 = "By: Justin Oblak";
+		console.log('here');
+
+		self.creditsSize = 24;
+
+		self.creditsX = this.pos.x - me.game.viewport.pos.x + 100;
+		self.creditsY = this.pos.y - me.game.viewport.pos.y + 100;
+
+		self.credits = new me.Font('century gothic', self.creditsSize, 'black');
+
+		var tween = new me.Tween(self)
+			.to({creditsX: self.creditsX,
+				 creditsY: -200,
+				 creditsSize: 24
+				}, 15000);
+		tween.start();
+
+	},
+
+	draw: function(context) {
+		this.credits.draw(context, this.credits1, this.creditsX, this.creditsY);
+		this.credits.draw(context, this.credits2, this.creditsX, this.creditsY + 50);
+		this.credits.set('century gothic', this.creditsSize, 'black');
+	}
 });
